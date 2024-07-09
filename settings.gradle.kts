bd86af67dd30e4528431c33322c393720c04371f
Rick Busarow <rickbusarow@gmail.com>
2024-07-09 12:31:10 -0500

    replace the deprecated Gradle Enterprise plugin with the develocity one

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,23 @@
  * limitations under the License.
  */
 
+import org.gradle.internal.impldep.org.eclipse.jgit.transport.ReceiveCommand.link
+
+/*
+ * Copyright (C) 2024 Rick Busarow
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 rootProject.name = "mahout"
 
 pluginManagement {
@@ -32,7 +49,7 @@ pluginManagement {
 }
 
 plugins {
-  id("com.gradle.enterprise") version "3.17.5"
+  id("com.gradle.develocity") version "3.17.5"
 }
 
 @Suppress("UnstableApiUsage")
@@ -44,17 +61,28 @@ dependencyResolutionManagement {
   }
 }
 
-gradleEnterprise {
+develocity {
   buildScan {
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
-    publishAlways()
 
-    // https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
-    tag(if (System.getenv("CI").isNullOrBlank()) "Local" else "CI")
+    val isCI = !System.getenv("CI").isNullOrEmpty()
+
+    uploadInBackground = !isCI
+    termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+    termsOfUseAgree = "yes"
+
+    capture {
+      testLogging = true
+      buildLogging = true
+      fileFingerprints = true
+    }
+
+    obfuscation {
+      hostname { "<hostName>" }
+      ipAddresses { listOf("<ip address>") }
+      username { "<username>" }
+    }
 
     val gitHubActions = System.getenv("GITHUB_ACTIONS")?.toBoolean() ?: false
-
     if (gitHubActions) {
       // ex: `octocat/Hello-World` as in github.com/octocat/Hello-World
       val repository = System.getenv("GITHUB_REPOSITORY")!!
