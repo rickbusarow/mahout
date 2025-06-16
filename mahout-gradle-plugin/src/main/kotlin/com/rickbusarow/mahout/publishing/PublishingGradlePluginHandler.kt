458512a40d139b399ae077703826ad2c14614b0d
Rick Busarow <rickbusarow@gmail.com>
2025-06-16 13:27:27 -0500

    feat(publishing): implement gradle plugin publishing extension

--- mahout-gradle-plugin/src/main/kotlin/com/rickbusarow/mahout/publishing/PublishingGradlePluginHandler.kt
+++ mahout-gradle-plugin/src/main/kotlin/com/rickbusarow/mahout/publishing/PublishingGradlePluginHandler.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Rick Busarow
+ * Copyright (C) 2025 Rick Busarow
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,7 +15,15 @@
 
 package com.rickbusarow.mahout.publishing
 
+import com.rickbusarow.kgx.isRootProject
+import com.rickbusarow.mahout.config.mahoutProperties
+import com.rickbusarow.mahout.conventions.applyBinaryCompatibility
+import com.rickbusarow.mahout.dokka.DokkatooConventionPlugin
+import modulecheck.utils.requireNotNull
 import org.gradle.api.NamedDomainObjectProvider
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.plugin.devel.GradlePluginDevelopmentExtension
 import org.gradle.plugin.devel.PluginDeclaration
 import javax.inject.Inject
 
@@ -27,10 +35,54 @@ public interface PublishingGradlePluginHandler : java.io.Serializable {
 }
 
 /** */
-public open class DefaultPublishingGradlePluginHandler @Inject constructor() :
-  PublishingGradlePluginHandler {
+public open class DefaultPublishingGradlePluginHandler @Inject constructor(
+  private val target: Project
+) : PublishingGradlePluginHandler {
 
-  override fun publishPlugin(pluginDeclaration: NamedDomainObjectProvider<PluginDeclaration>) {
-    TODO()
+  override fun publishPlugin(
+    pluginDeclaration: NamedDomainObjectProvider<PluginDeclaration>
+  ) {
+
+    target.plugins.apply("java-gradle-plugin")
+    target.plugins.apply("com.gradle.plugin-publish")
+    target.plugins.apply("com.vanniktech.maven.publish.base")
+    target.plugins.apply(DokkatooConventionPlugin::class.java)
+
+    require(target.pluginManager.hasPlugin("org.jetbrains.kotlin.jvm"))
+
+    configurePublishPlugin(pluginDeclaration)
+  }
+
+  private fun configurePublishPlugin(
+    pluginDeclaration: NamedDomainObjectProvider<PluginDeclaration>
+  ) {
+    if (target.isRootProject()) {
+      target.applyBinaryCompatibility()
+    }
+
+    target.plugins.withId("com.gradle.plugin-publish") {
+
+      pluginDeclaration.configure { declaration ->
+
+        target.extensions.configure(
+          GradlePluginDevelopmentExtension::class.java
+        ) { pluginDevelopmentExtension ->
+
+          pluginDevelopmentExtension.website.set(target.mahoutProperties.publishing.pom.url)
+          pluginDevelopmentExtension.vcsUrl.set(target.mahoutProperties.publishing.pom.scm.url)
+        }
+      }
+    }
+
+    target.gradlePublishingExtension.publications
+      .withType(MavenPublication::class.java)
+      .configureEach { publication ->
+
+        publication.pom.description.set(
+          pluginDeclaration.map {
+            it.description.requireNotNull { "A plugin description is required." }
+          }
+        )
+      }
   }
 }
